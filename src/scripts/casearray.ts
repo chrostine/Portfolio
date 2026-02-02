6e778158148f5f507ab4156392b62599d2ab23fd
chrostine <christineswierkosz@gmail.com>
2026-02-02 18:57:00 +0100

    added datavisualiserung

---
+++ src/scripts/casearray.ts
@@ -0,0 +1,47 @@
+
+
+export type Case = {
+  slug: string;
+  titel: string;
+  undertitel: string;
+  date: string;
+  billede: string;
+  gif: string;
+  text: string[];
+  imgdisplay: string[];
+};
+
+export const casearray: Case[] = [
+  {
+    slug: "pixeline",
+    titel: "maskotgenerator: Pixeline",
+    undertitel: "lorem ipsum",
+    date: "2025-11-05",
+    billede: "/cases/pixeline/pixeline.png",
+    gif: "/cases/pixeline/pixeline.gif",
+    text: [
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+    ],
+    imgdisplay: [
+      "/cases/pixeline/pixeline.png",
+      "/cases/pixeline/pixeline.png",
+      "/cases/pixeline/pixeline.png",
+    ],
+  },
+    {
+    slug: "handicaploftet",
+    titel: "Datavisualisering af Handicapløftet",
+    undertitel: "lorem ipsum",
+    date: "2025-12-05",
+    billede: "/cases/handicaploftet/danmarkskort.png",
+    gif: "/cases/pixeline/pixeline.gif",
+    text: [
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+    ],
+    imgdisplay: [
+        "/cases/handicaploftet/danmarkskort.png",
+    ],
+  },
+];
