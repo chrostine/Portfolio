721c94dea18dfc20cd6bc9fdbe60c9ebf8c55f18
chrostine <christineswierkosz@gmail.com>
2026-02-23 12:06:53 +0100

    added two new cases

--- src/scripts/casearray.ts
+++ src/scripts/casearray.ts
@@ -14,23 +14,30 @@ export const casearray: Case[] = [
     slug: "pixeline",
     titel: "maskotgenerator: Pixeline",
     undertitel: [
-      "maskotgenerator: Pixeline",
-      "alt hvad jeg har lavet er skolearbejde",
+      "Træning af Flux LoRA til at kunne genskabe pixeline på billeder",
     ],
     date: "2025-11-05",
     billede: "/cases/pixeline/pixeline.png",
     gif: "/cases/pixeline/pixeline.gif",
     text: [
-        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+      "Dette projekt er udarbejdet som led i undervisning og er udelukkende til ikke-kommercielt brug. Alle rettigheder tilhører de oprindelige rettighedshavere.",
+      `“[Brand] skal bruge en ny kampagnewebsite. Det er vigtigt for [Brand], at [Maskot] indgår i alt marketingsmateriale. Deadline er snart, så deres marketingafdeling, kan ikke nå at bygge websitet, eller at få [Maskot] med.`,
+      `Ved at bruge AI, kan vi automatisere og effektivisere produktionen af indhold, og derved åbne op for en mere agil og fri proces.”`,
+      "Pixeline, tekst, ikoner og baggrundsbilleder er genereret af kunstig intelligens.",
     ],
     imgdisplay: [
-      "/cases/pixeline/pixeline.png",
-      "/cases/pixeline/pixeline.png",
-      "/cases/pixeline/pixeline.png",
+      "/cases/pixeline/pixeline01.png",
+      "/cases/pixeline/pixeline02.png",
+      "/cases/pixeline/pixeline03.png",
+      "/cases/pixeline/pixeline04.png",
+      "/cases/pixeline/pixeline05.png",
+      "/cases/pixeline/pixeline06.png",
+      "/cases/pixeline/pixeline07.png",
+      "/cases/pixeline/pixeline08.png",
+      "/cases/pixeline/pixeline09.png",
     ],
   },
-    {
+  {
     slug: "handicaploftet",
     titel: "Datavisualisering af Handicapløftet",
     undertitel: [
@@ -48,19 +55,18 @@ export const casearray: Case[] = [
         "/cases/handicaploftet/danmarkskort.png",
     ],
   },
-      {
+  {
     slug: "munich",
     titel: "City Branding: Munich",
     undertitel: [
-      "Lorem ipsum dolor sit amet",
-      "Quisque faucibus ex sapien",
+      "Dynamisk visuel identitet",
     ],
-    date: "2026-01-23",
+    date: "2026-02-06",
     billede: "/cases/munich/pattern.png",
     gif: "/cases/munich/munich.gif",
     text: [
-        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+      "Jeg har designet en visuel identitet til München med afsæt i byens flag, der består af sorte og gule romber.",
+      "Romben er splittet op i trekanter, som fungerer som identitetens modulære byggesten. Trekanterne kan kombineres frit og skaber et dynamisk, foranderligt system."
     ],
     imgdisplay: [
         "/cases/munich/munich01.png",
@@ -70,6 +76,43 @@ export const casearray: Case[] = [
         "/cases/munich/munich05.png",
         "/cases/munich/munich06.png",
         "/cases/munich/munich07.png",
+    ],
+  },
+  {
+    slug: "pi-day",
+    titel: "Plakatgenerator til Pi-dag",
+    undertitel: [
+      "Interaktiv plakat"
+    ],
+    date: "2024-11-29",
+    billede: "/cases/piday/piday01.png",
+    gif: "/cases/piday/piday.png",
+    text: [
+      `Motivet på plakaten er inspireret af pi. Udvalgte variabler i sketchen kan styres med mus og tastatur, og plakaten kan eksporteres som png ved at trykke “v”.`,
+      "Sketchen er lavet i Processing."
+    ],
+    imgdisplay: [
+        "/cases/piday/piday_right.gif",
+        "/cases/piday/piday_rotate.gif",
+        "/cases/piday/piday_zoom.gif",
+
+    ],
+  },
+  {
+    slug: "sakura island",
+    titel: "Blender: Sakura island",
+    undertitel: [
+    ],
+    date: "2024-11-15",
+    billede: "/cases/sakura/sakura.png",
+    gif: "/cases/sakura/sakura.gif",
+    text: [
+      "Visualisering af min egen ø i et 3D-miljø med afsæt i form, farve og komposition."
+    ],
+    imgdisplay: [
+      "/cases/sakura/sakura1.gif",
+      "/cases/sakura/sakura2.gif",
+      "/cases/sakura/sakura3.gif",
     ],
   },
 ];
